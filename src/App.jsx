782db7f44b8c0de5f499dad4febd06e88fe42e26
MaishaFN <jose.maisha@gmail.com>
2023-06-01 17:33:46 +0200

    Routes in App.jsx and Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,45 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import {Routes, Route} from "react-router-dom"
-import Signup from './pages/auth/Signup'
-import Login from './pages/auth/Login'
-import Error from './pages/error/Error'
-import NotFound from './pages/error/NotFound'
-import Home from './pages/Home'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
+import Error from "./pages/error/Error";
+import NotFound from "./pages/error/NotFound";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import Profile from "./pages/Profile";
+import UserInfo from "./pages/UserInfo";
+import GroupList from "./pages/groups/GroupList";
+import GroupDetails from "./pages/groups/GroupDetails";
+import GameList from "./pages/games/GameList";
+import GameDetails from "./pages/games/GameDetails";
 
 function App() {
-
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
       <Routes>
-        <Route path='/auth/signup' element={<Signup/>}/>
-        <Route path='/auth/login' element={<Login/>}/>
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/auth/login" element={<Login />} />
 
-        <Route path='/' element={<Home/>}/>
+        <Route path="/admin" element={<Admin />} />
 
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/user/:userId" element={<UserInfo />} />
 
-        <Route path='/error' element={<Error/>}/>
-        <Route path='*' element={<NotFound/>}/>
+        <Route path="/group/list" element={<GroupList />} />
+        <Route path="/group/:groupId" element={<GroupDetails />} />
+
+        <Route path="/game/list" element={<GameList />} />
+        <Route path="/game/:gameId/details" element={<GameDetails />} />
+
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,18 @@ export default function Navbar() {
     <div>
         <Link to="/auth/login">Login</Link>
         <Link to="/auth/signup">Signup</Link>
+
+        <Link to="/admin">Admin</Link>
+
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/user/:userId">User</Link>
+
+        <Link to="/group/list">Group list</Link>
+        <Link to="/group/:groupId">Group</Link>
+
+        <Link to="/game/list">Games list</Link>
+        <Link to="/game/:gameId/details">Game</Link>
     </div>
   )
 }
