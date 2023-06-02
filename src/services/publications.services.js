8fb4be65fc5bd4c388f93f134a464f3dad447355
MaishaFN <jose.maisha@gmail.com>
2023-06-02 15:57:54 +0200

    Profile.jsx done

--- src/services/publications.services.js
+++ src/services/publications.services.js
@@ -1,7 +1,6 @@
 import service from "./config.services";
 
 const allPublications = () => {
-<<<<<<< HEAD
     return service.get("/publication");
   };
   
@@ -15,21 +14,6 @@ const allPublications = () => {
   
   const friendsPublication = () => {
     return service.get(`/publication/friendList`);
-=======
-    return service.get("/publications");
-  };
-  
-  const newPublication = () => {
-    return service.post("/publications");
-  };
-  
-  const deletePublication = (deletePublicationId) => {
-    return service.delete(`/publications/${deletePublicationId}`);
-  };
-  
-  const friendsPublication = () => {
-    return service.get(`/publications/friendList`);
->>>>>>> b0e798b7d176730146897d4af15839bef6ad4930
   };
   
   export { allPublications, newPublication, deletePublication, friendsPublication };
