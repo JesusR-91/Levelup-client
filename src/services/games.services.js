0bf900be1592435a28e6d2ec7a5d98a23d771466
MaishaFN <jose.maisha@gmail.com>
2023-06-02 16:01:13 +0200

    minor changes

--- src/services/games.services.js
+++ src/services/games.services.js
@@ -4,13 +4,6 @@ const gameListService = (page) => {
   return service.get(`/game/list/${page}`);
 };
 
-const gameDetailsService = (gameId) => {
-  return service.get(`/games/${gameId}/details`);
-const gameListService = (page) =>{
-    return service.get(`/games/list/${page}`)
-};
-
-export { gameListService, gameDetailsService };
 const gameDetailsService = (gameId) =>{
     return service.get(`/games/${gameId}/details`)
 };
