1e4211fb7c23defa4c83a39b6522e13492017bf3
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-06-02 17:35:23 +0200

    admin services created

--- src/pages/groups/GroupDetails.jsx
+++ src/pages/groups/GroupDetails.jsx
@@ -4,12 +4,14 @@ import { groupDetailsService } from "../../services/group.services";
 
 export default function GroupDetails() {
   const [group, setGroup] = useState();
+  const [owner, setOwner] = useState();
   const {groupId} = useParams();
 
   const navigate = useNavigate()
   const getData = async () => {
     try {
       const response = await groupDetailsService(groupId);
+      const ownerResponse = await us
       setGroup(response.data)
     } catch (error) {
       console.log(error);
@@ -24,6 +26,9 @@ export default function GroupDetails() {
       <h3>{group.name}</h3>
 
       <p>{group.description}</p>
+
+      <h3>Owner</h3>
+
 
       <h3>Users:</h3>
       {group.participants.map(user => (<Link to={`/user/${user._id}/details`} key={user._id}>{user.username}</Link>))}
