eae9ef431a8ab33bdc6165014888df206bfc3cc9
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-06-05 09:23:55 +0200

    minor changes, minor bug

--- src/pages/groups/GroupDetails.jsx
+++ src/pages/groups/GroupDetails.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { groupDetailsService } from "../../services/group.services";
 import { friendInfoService } from "../../services/user.services";
@@ -7,24 +7,27 @@ export default function GroupDetails() {
   const [group, setGroup] = useState();
   const [owner, setOwner] = useState();
   const [isLoading, setIsLoading] = useState(true);
-  const {groupId} = useParams();
+  const { groupId } = useParams();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const getData = async () => {
     try {
       const response = await groupDetailsService(groupId);
-      setGroup(response.data)
-      const ownerResponse = await friendInfoService(group.owner);
-      setOwner(ownerResponse.data)
-      setIsLoading(false)
+      setGroup(response.data);
+      const ownerResponse = await friendInfoService(response.data.owner);
+      setOwner(ownerResponse.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
-      navigate("/error");           
+      navigate("/error");
     }
-  }
+  };
 
-  useEffect(() => {getData()}, [])
-  
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return !isLoading ? (
     <div>
       <h3>{group.name}</h3>
@@ -33,10 +36,14 @@ export default function GroupDetails() {
 
       <h3>Owner: {owner.username}</h3>
 
-
       <h3>Users:</h3>
-      {group.participants.map(user => (<Link to={`/user/${user._id}/details`} key={user._id}>{user.username}</Link>))}
+      {group.participants.map((user) => (
+        <Link to={`/user/${user._id}/details`} key={user._id}>
+          {user.username}
+        </Link>
+      ))}
     </div>
-    
-  ) : <h3>Loading...</h3>
+  ) : (
+    <h3>Loading...</h3>
+  );
 }
