67b1c1b31c4244af978c3d0554508a6b62dca19d
MaishaFN <jose.maisha@gmail.com>
2023-06-06 09:38:00 +0200

    Delete user and group with admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,5 @@
-import { getAllGroupsService } from "../services/admin.services";
-import { getAllUserService } from "../services/admin.services";
-import { useNavigate } from "react-router-dom";
+import { getAllGroupsService, getAllUserService, deleteUserService, deleteGroupService } from "../services/admin.services";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function Admin() {
@@ -8,11 +7,13 @@ export default function Admin() {
   const [groups, setGroups] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
+  const params = useParams()
 console.log(groups)
   const getData = async () => {
     try {
       const users = await getAllUserService();
       const groups = await getAllGroupsService();
+
       setUsers(users.data);
       setGroups(groups.data);
       setIsLoading(false);
@@ -24,20 +25,38 @@ console.log(groups)
   useEffect(() => {
     getData();
   }, []);
-
+  const handleDeleteGroup = async (id)=> {
+    try {
+      await deleteGroupService(id)
+      navigate("/admin")
+    } catch (error) {
+      console.log(error)
+      navigate("/admin")
+    }
+  }
+  const handleDeleteUser = async (id)=> {
+    try {
+      await deleteUserService(id)
+      navigate("/admin")
+    } catch (error) {
+      console.log(error)
+      navigate("/admin")
+    }
+  }
+  
   return !isLoading ? (
     <div>
       <h3>All users:</h3>
         {users.map((eachUser)=>(
       <div key={eachUser._id}>
-          <h4>{eachUser.username} <button>Delete</button></h4>
+          <h4>{eachUser.username} <button onClick={()=>{handleDeleteUser(eachUser._id)}}>Delete</button></h4>
       </div>
       ))}
       <br/>
       <h3>All groups:</h3>
       {groups.map((eachGroup)=>
       <div key={eachGroup._id}>
-          <h4>{eachGroup.name} <button>Delete</button></h4>
+          <h4>{eachGroup.name} <button onClick={()=>{handleDeleteGroup(eachGroup._id)}}>Delete</button></h4>
       </div>
       )}
       </div>
