3dab29c9d7dbb6b6080449bc8c4e833d3eb9572e
MaishaFN <jose.maisha@gmail.com>
2023-06-06 14:13:51 +0200

    solve bug

--- src/components/AddUserGroup.jsx
+++ src/components/AddUserGroup.jsx
@@ -1,30 +1,38 @@
-import {groupAddUserService} from "../../services/group.services";
+import { groupAddUserService } from "../services/group.services";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function AddUserGroup() {
-
-    const getData = async () => {
-        try {
-          const users = await groupAddUserService();
-          setUsers(users.data);
-        } catch (error) {
-          console.log(error);
-          navigate("/error");
-        }
-      };
-      useEffect(() => {
-        getData();
-      }, []);
-    const handleAddUser = async (id) => {
-        try {
-          await groupAddUserService(id)
-        } catch (error) {
-          console.log(error)
-          navigate("/error")
-        }
-      }
+  const navigate = useNavigate()
+  const getData = async () => {
+    try {
+      const users = await groupAddUserService();
+      setUsers(users.data);
+    } catch (error) {
+      console.log(error);
+      navigate("/error");
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const handleAddUser = async (id) => {
+    try {
+      await groupAddUserService(id);
+    } catch (error) {
+      console.log(error);
+      navigate("/error");
+    }
+  };
   return (
-    <div><button onClick={()=>{handleAddUser(eachUser._id)}}>Add User</button></div>
-  )
+    <div>
+      <button
+        onClick={() => {
+          handleAddUser(eachUser._id);
+        }}
+      >
+        Add User
+      </button>
+    </div>
+  );
 }
