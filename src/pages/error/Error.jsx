377c6b189c9b45b3a51c18b5f1f4b212df161674
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-06-09 11:39:53 +0200

    error images imports

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,9 +1,11 @@
+//IMPORTS
 import { Link } from "react-router-dom";
+import image from "../../assets/itsatrap.png";
 
 export default function Error() {
   return (
-    <div>
-     <Link to={"/"} ><img src={"../public/itsatrap.png"} alt="Error 400"/></Link>
+    <div style={{display:"flex", justifyContent:"center", justifyItems:"center"}}>
+     <Link to={"/"} ><img src={image} alt="Error 400"/></Link>
     </div>
   )
 }
--- src/pages/error/NotFound.jsx
+++ src/pages/error/NotFound.jsx
@@ -1,9 +1,11 @@
+//IMPORT
 import { Link } from "react-router-dom";
+import image from "../../assets/gandalf.jpg";
 
 export default function NotFound() {
   return (
-    <div>
-     <Link to={"/"} ><img src={"../public/gandalf.jpg"} alt="Error 400"/></Link>
+    <div style={{display:"flex", justifyContent:"center", justifyItems:"center"}}>
+     <Link to={"/"} ><img src={image} alt="Error 400"/></Link>
     </div>
   )
 }
